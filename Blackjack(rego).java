74756af4b0062ed282a59de4fdbd26fd2e8dde55
regooooo <solayvar@addu.edu.ph>
2025-04-16 16:56:08 +0800

    Add files via upload

---
+++ Blackjack(rego).java
@@ -0,0 +1,169 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blackjack;
+import java.util.*;
+
+/**
+ *
+ * @author regor
+ */
+public class Blackjack {
+    static int cardcounter = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double balance = 500.0;
+        double bet;
+        String choice;
+        balance = play(balance);
+       
+        do{
+        cardcounter = 0;
+        System.out.printf("Balance: $%.2f%n", balance);
+        System.out.println("\nDo you want to play again?\n(1) Play (2) Exit\n");
+        choice = scanner.next();
+        if (choice.equals("1")){
+            balance = play(balance);
+        }
+        else{
+            System.out.println("Thank you for playing! Come again!");
+        }
+        }while(choice.equals("12"));
+    }
+    
+    public static double BJ(double bet, double balance){
+        Scanner scanner = new Scanner(System.in);
+        int vdealer;
+        int vplayer;
+        int hidden;
+        String choice;
+        System.out.printf("Bet with the amount of $%.2f confirmed! The remaining balance is $%.2f.%n%n", bet, balance);
+        vdealer = random(); 
+        hidden = random();
+        vplayer = random() + random();
+        do{
+            
+            System.out.println(dealer(vdealer));
+            System.out.println(player(vplayer));
+            
+            if(vplayer==21){
+                pwin(vdealer, hidden, vplayer, bet);
+                return bet*2;
+            }       
+            
+            if((vdealer + hidden) ==21){
+                    plose(vdealer, hidden, vplayer);
+                    return 0;
+            }
+            
+            System.out.println("(1) Hit (2) Stand\n");
+            choice = scanner.next();
+            System.out.println("");
+            if(!(choice.equalsIgnoreCase("1")|| choice.equalsIgnoreCase("2"))){
+                do{
+                    System.out.println("Please enter either 1 or 2");
+                    choice = scanner.next();
+                }while(choice.equalsIgnoreCase("12"));
+            }
+            if (choice.equals("2")){
+                vdealer = stand(vdealer+hidden);
+                if(vdealer>21 || vdealer<vplayer){
+                    pwin(vdealer, hidden, vplayer, bet);
+                    return bet*2;
+                }
+                else if(vdealer==21 || vdealer>vplayer){
+                    plose(vdealer, hidden, vplayer);
+                    return 0;   
+                }
+                else if(vplayer == vdealer){
+                    System.out.printf("Tie! The bet of $%.2f will be returned.%n", bet);
+                    return bet;
+                }
+                
+            }
+            if(choice.equals("1")){
+                vplayer += random();
+                if(vplayer>21){
+                    plose(vdealer, hidden, vplayer);
+                    return 0;
+                }
+                if(vplayer==21){
+                    pwin(vdealer, hidden, vplayer, bet);
+                    return bet*2; 
+                }
+            }
+            
+        }while(choice.equalsIgnoreCase("1") || choice.equalsIgnoreCase("2"));
+        return 0;
+    }
+    
+    public static int random() {
+    
+    int[] values = {
+    10, 3, 1, 10, 8, 2, 6, 10,
+    5, 9, 7, 2, 4, 10, 10, 10,
+    10, 10, 5, 4, 7, 10, 10, 9,
+    8, 6, 11, 7, 10, 3, 4, 2,
+    11, 10, 3, 8, 9, 6, 5, 10,
+    2, 4, 3, 7, 6, 8, 1, 9,
+    5, 10, 10, 10};
+
+    Random random = new Random();
+    int num = values[random.nextInt(values.length)];
+    System.out.printf("Card dealt: %s%n", (cardcounter!=1 ? num : "Hidden"));
+    cardcounter++;
+    return num;
+}
+    
+    public static void pwin(int vdealer, int hidden, int vplayer, double bet){
+        System.out.println(dealerf(vdealer, hidden));
+        System.out.println(player(vplayer));
+        System.out.printf("You Win! Congratulations for winning $%.2f%n", bet*2);
+
+    }
+
+    public static void plose(int vdealer, int hidden, int vplayer){
+        System.out.println(dealerf(vdealer, hidden));
+        System.out.println(player(vplayer));
+        if(vplayer>21){
+        System.out.println("Bust!\n");
+        }
+        else System.out.println("You lose!\n"); // turn to ternary
+    }
+
+    public static String dealer(int vdealer){
+        return String.format("---------------------%nDealer: %d + ?%n", vdealer);
+    }
+    
+    public static String dealerf(int vdealer, int hidden){
+        return String.format("---------------------%nDealer: %d Hidden: %d %n", vdealer, hidden);
+    }
+    
+    public static String player(int vplayer){
+        return String.format("Player: %d%n---------------------%n", vplayer);
+    }
+
+    public static int stand(int value){
+        if(value<18)
+        do{
+            value+=random();
+        }while(value<18);
+        return value;
+    }
+    
+    public static double play(double balance){
+        double bet;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter bet.");
+        do{
+            bet = scanner.nextInt();
+            if(balance<bet){
+                System.out.println("Please enter sufficient amount.");
+            }
+        }while(balance<bet);
+        balance-=bet;
+        return balance = balance + BJ(bet, balance);
+        
+    }
+}
